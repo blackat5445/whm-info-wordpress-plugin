ad4f8d832da8602e5190e7fa185f4327b47307cc
blackat5445 <kfalahati49@gmail.com>
2025-11-07 15:50:41 +0100

    RELEASE: V-1.0.1

--- whm-info.php
+++ whm-info.php
@@ -3,7 +3,7 @@
  * Plugin Name: WHM Info
  * Plugin URI: https://www.agenziamagma.it
  * Description: Key plugin to connect to WHM software and show the status of the services.
- * Version: 0.2.0
+ * Version: 1.0.1
  * Author: Kasra Falahati, Agenzia Magma
  * Author URI: https://www.kasra.eu
  * License: GPL-2.0+
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('WHMIN_VERSION', '0.2.0');
+define('WHMIN_VERSION', '1.0.1');
 define('WHMIN_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WHMIN_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('WHMIN_PLUGIN_BASENAME', plugin_basename(__FILE__));
